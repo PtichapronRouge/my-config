cafee2f0a36b77ae95b1bf4ce405be6fbcfe58bf
Benjamin Mouscadet <benjamin.mouscadet@supelec.fr>
2018-12-06 14:28:26 +0100

    Added vundle download & execution

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Vimrc initialization
 ln -s ~/my-config/vim/vimrc ~/.vimrc
+
+# Vundle download
+git clone https://github.com/VundleVim/Vundle.vim.git ~/my-config/vim/bundle/Vundle.vim
+# Vundle launch
+vim +PluginInstall +qall
