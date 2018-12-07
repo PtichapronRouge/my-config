e0915292e806e0c847e58e0383c0e48097b41676
Benjamin Mouscadet <benjamin.mouscadet@supelec.fr>
2018-12-07 16:38:47 +0100

    Added minor improvements to init.sh file

--- init.sh
+++ init.sh
@@ -5,6 +5,16 @@ ln -f -s ~/my-config/vim ~/.vim
 ln -f -s ~/.vim/vimrc ~/.vimrc
 
 # Vundle download
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+VUNDLE_PATH=~/.vim/bundle/Vundle.vim
+
+if [ -e $VUNDLE_PATH ]; then
+    echo "Vundle.vim repository found. Pulling..."
+    git -C $VUNDLE_PATH pull
+else
+    echo "Cloning Vundle.vim repository"
+    git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_PATH
+fi
+echo "Done"
+
 # Vundle launch
 vim +PluginInstall +qall
